8838f79d047192c22a79cd2cdd9b0899e80db1de
jpratt-mru <jpratt@mtroyal.ca>
2018-06-14 11:22:04 -0600

    ZoomIt checkin

---
+++ zoomit.4.50.0.20160210/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Get-ChocolateyUnzip "$toolsDir\ZoomIt.7z" $toolsDir
+
+Add-ShortcutToStartMenu "C:\ProgramData\chocolatey\bin\ZoomIt.exe" "ZoomIt" 3
+
+$regRoot = 'Software\Sysinternals\ZoomIt'
+Add-RegistryValueForEveryUser $regRoot 'EulaAccepted' 1 "Dword"
+Add-RegistryValueForEveryUser $regRoot 'ToggleKey' 624 "Dword"
+
