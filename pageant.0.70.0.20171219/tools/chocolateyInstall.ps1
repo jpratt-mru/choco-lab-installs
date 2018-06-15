5e01d2851e24e5f31b118d79ad6602693ab1f8d6
jpratt-mru <jpratt@mtroyal.ca>
2018-06-15 11:08:14 -0600

    Pageant done.

---
+++ pageant.0.70.0.20171219/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Get-ChocolateyUnzip "$toolsDir\pageant.7z" $toolsDir
+
+Add-ShortcutToStartMenu "C:\ProgramData\chocolatey\bin\pageant.exe" "Pageant"
+
+
