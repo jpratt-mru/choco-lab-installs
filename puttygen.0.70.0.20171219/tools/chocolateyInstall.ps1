a232b0b576013c93be9ea7d65b2d8e5f4d3daee8
jpratt-mru <jpratt@mtroyal.ca>
2018-06-15 10:59:17 -0600

    Puttygen install done.

---
+++ puttygen.0.70.0.20171219/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Get-ChocolateyUnzip "$toolsDir\puttygen.7z" $toolsDir
+
+Add-ShortcutToStartMenu "C:\ProgramData\chocolatey\bin\puttygen.exe" "PuTTYgen"
+
+
