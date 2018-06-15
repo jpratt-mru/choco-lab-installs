a01a20ebdb12071026a4b54ca09965d181b12c9b
jpratt-mru <jpratt@mtroyal.ca>
2018-06-15 12:22:48 -0600

    JustColorPicker done.

---
+++ justcolorpicker.5.1.0.20180615/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Get-ChocolateyUnzip "$toolsDir\jcpicker.7z" $toolsDir
+
+$regRoot = 'Software\Microsoft\Windows\CurrentVersion\Explorer\DontShowMeThisDialogAgain'
+$GUID = "{46FF719E-48AA-4FCC-80F9-B376FBF72F6F}"
+Add-RegistryValueForEveryUser $regRoot `"$GUID`" 'NO' 'String'
+
+Add-ShortcutToStartMenu "C:\ProgramData\chocolatey\bin\jcpicker.exe" "JustColorPicker"
+
