5a04491a09b53cc6c8cdaddf5857eb3f5e9da1ca
jpratt-mru <jpratt@mtroyal.ca>
2018-06-18 15:46:13 -0600

    Clean up.

--- justcolorpicker.5.1.0.20180615/tools/chocolateyInstall.ps1
+++ justcolorpicker.5.1.0.20180615/tools/chocolateyInstall.ps1
@@ -6,7 +6,9 @@ Get-ChocolateyUnzip "$toolsDir\jcpicker.7z" $toolsDir
 
 $regRoot = 'Software\Microsoft\Windows\CurrentVersion\Explorer\DontShowMeThisDialogAgain'
 $GUID = "{46FF719E-48AA-4FCC-80F9-B376FBF72F6F}"
-Add-RegistryValueForEveryUser $regRoot `"$GUID`" 'NO' 'String'
+
+# we do the weird `" `" thing because of the {} in the GUID
+Add-RegistryEntryForEveryUser $regRoot `"$GUID`" 'NO' 'String'   
 
 Add-ShortcutToStartMenu "C:\ProgramData\chocolatey\bin\jcpicker.exe" "JustColorPicker"
 
--- justcolorpicker.5.1.0.20180615/tools/chocolateyuninstall.ps1
+++ justcolorpicker.5.1.0.20180615/tools/chocolateyuninstall.ps1
@@ -1,3 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
 Remove-ShortcutFromStartMenu 'JustColorPicker'
+
+$regRoot = 'Software\Microsoft\Windows\CurrentVersion\Explorer\DontShowMeThisDialogAgain'
+$regEntry = "{46FF719E-48AA-4FCC-80F9-B376FBF72F6F}"
+Remove-RegistryEntryForEveryUser $regRoot `"$regEntry`"
