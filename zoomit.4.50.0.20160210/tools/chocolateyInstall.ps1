1b1753ebfcc6997be1c1958456b99cc42b6598d5
jpratt-mru <jpratt@mtroyal.ca>
2018-06-18 15:45:27 -0600

    Clean up.

--- zoomit.4.50.0.20160210/tools/chocolateyInstall.ps1
+++ zoomit.4.50.0.20160210/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Get-ChocolateyUnzip "$toolsDir\ZoomIt.7z" $toolsDir
 
 $regRoot = 'Software\Sysinternals\ZoomIt'
-Add-RegistryValueForEveryUser $regRoot 'EulaAccepted' 1 "Dword"
-Add-RegistryValueForEveryUser $regRoot 'ToggleKey' 624 "Dword"
+Add-RegistryEntryForEveryUser $regRoot 'EulaAccepted' 1 "Dword"
+Add-RegistryEntryForEveryUser $regRoot 'ToggleKey' 624 "Dword"
 
 Add-ShortcutToStartMenu "C:\ProgramData\chocolatey\bin\ZoomIt.exe" "ZoomIt"
 
