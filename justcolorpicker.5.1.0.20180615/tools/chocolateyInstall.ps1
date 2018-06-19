2f39d0ca2796ee1f71ee3d859a274e0238915c07
jpratt-mru <jpratt@mtroyal.ca>
2018-06-19 10:56:49 -0600

    Update nuspec and repack.

--- justcolorpicker.5.1.0.20180615/tools/chocolateyInstall.ps1
+++ justcolorpicker.5.1.0.20180615/tools/chocolateyInstall.ps1
@@ -7,6 +7,7 @@ Get-ChocolateyUnzip "$toolsDir\jcpicker.7z" $toolsDir
 $regRoot = 'Software\Microsoft\Windows\CurrentVersion\Explorer\DontShowMeThisDialogAgain'
 $GUID = "{46FF719E-48AA-4FCC-80F9-B376FBF72F6F}"
 
+# registry hack to stop the nag on exit...
 # we do the weird `" `" thing because of the {} in the GUID
 Add-RegistryEntryForEveryUser $regRoot `"$GUID`" 'NO' 'String'   
 
