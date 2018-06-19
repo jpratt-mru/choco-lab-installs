99c05982795b67a0308d3fdd21329de5a4be951b
jpratt-mru <jpratt@mtroyal.ca>
2018-06-19 12:38:06 -0600

    In goes Isoplot.

---
+++ isoplot.1.0.0.20180619/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Get-ChocolateyUnzip "$toolsDir\isoplot.7z" $toolsDir
+
+Add-ShortcutToStartMenu "C:\ProgramData\chocolatey\lib\isoplot\tools\activate_isoplot.vbs" "Isoplot"
+
+$command = @"
+icacls "C:\ProgramData\chocolatey\lib\isoplot\tools" /grant Users:(OI)(CI)M
+"@
+
+Start-Process 'cmd' -ArgumentList "/c $command" -Wait
