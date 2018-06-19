f4e87fb5a435bf9dc29925e04ba11adebbd7b104
jpratt-mru <jpratt@mtroyal.ca>
2018-06-19 11:53:18 -0600

    Update nuspec and repack.

---
+++ kitty.0.70.0.20180619/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Get-ChocolateyUnzip "$toolsDir\kitty.7z" $toolsDir
+
+Add-ShortcutToStartMenu "C:\ProgramData\chocolatey\bin\kitty_portable.exe" "INS" 0 "-load INS"
