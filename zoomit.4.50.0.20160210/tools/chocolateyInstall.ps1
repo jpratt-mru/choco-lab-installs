a04a652537000728973ae384779e6382443a7ffd
jpratt-mru <jpratt@mtroyal.ca>
2018-06-19 10:27:01 -0600

    Add comments.

--- zoomit.4.50.0.20160210/tools/chocolateyInstall.ps1
+++ zoomit.4.50.0.20160210/tools/chocolateyInstall.ps1
@@ -5,7 +5,11 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Get-ChocolateyUnzip "$toolsDir\ZoomIt.7z" $toolsDir
 
 $regRoot = 'Software\Sysinternals\ZoomIt'
-Add-RegistryEntryForEveryUser $regRoot 'EulaAccepted' 1 "Dword"
+
+# Turn off license agreement nag
+Add-RegistryEntryForEveryUser $regRoot 'EulaAccepted' 1 "Dword" 
+
+# Change zoom hotkey to Ctrl + F1
 Add-RegistryEntryForEveryUser $regRoot 'ToggleKey' 624 "Dword"
 
 Add-ShortcutToStartMenu "C:\ProgramData\chocolatey\bin\ZoomIt.exe" "ZoomIt"
