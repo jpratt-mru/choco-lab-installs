3d924ff4ad1442b9accc9f66676d5df9f7a02379
jpratt-mru <jpratt@mtroyal.ca>
2018-06-21 15:20:45 -0600

    Get uninstall working

---
+++ brackets.1.13.20180621/tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+Remove-ShortcutFromStartMenu 'Brackets'
+Remove-ResourceForEveryUser 'AppData\Roaming\Brackets'
+
+$ToRun = @"
+MsiExec.exe /X{9CB3A036-0B7E-49B7-A60B-291E245CA6B2} /qn
+"@
+
+Start-Process 'cmd' -ArgumentList "/c $ToRun" -Wait
